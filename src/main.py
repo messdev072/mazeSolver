6e2aa3157a985bf2d2387bec92d9552a2c4ed23d
huytd072 <huytd072@gmail.com>
2024-07-23 20:17:39 +0700

    draw line

--- src/main.py
+++ src/main.py
@@ -1,3 +1,9 @@
-from graphics import Window
-win = Window(800, 600)
-win.wait_for_close()
+from graphics import Window, Line, Point
+
+def main():
+    win = Window(800, 600)
+    l = Line(Point(50, 50), Point(400, 400))
+    win.draw_line(l, "black")
+    win.wait_for_close()
+
+main()
